40cb502ddbc6fd00247aa7fd1292d4c0921dbbb3
myline70 <myline70@gmail.com>
2025-04-10 13:05:46 +0400

    Update main.py
    التعديل ليتناسب مع ماك

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from ui import MainWindow
-from record_question import RecordQuestion
-from speech_recognition_helper import SpeechRecognitionHelper
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
